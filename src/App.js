5dbcf2a37e6ba8059ee5199eb50aa2d249b12544
om0201 <74784928+om0201@users.noreply.github.com>
2020-12-15 08:53:33 +0530

    feat: added input/output

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [input, setInput] = useState("");
+
+  function activityHandler() {
+    var input = event.target.value;
+    setInput(input);
+  }
+
   return (
     <div className="App">
       <h1>Activities</h1>
+      <input onChange={activityHandler}></input>
+      <div>{input}</div>
     </div>
   );
 }
