a7c022057f03380fd2e35f8fc6dde3ab72f5853e
om0201 <74784928+om0201@users.noreply.github.com>
2020-12-15 09:08:57 +0530

    refactor: added database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const activities = {
+  "🤺": "fencing",
+  "🏇": "horse racing",
+  "⛷️": "skiing",
+  "🏂": "snowboarding",
+  "🏌️": "golf",
+  "🏄": "surfing",
+  "🚣": "boating",
+  "🏊": "swimming",
+  "🏋️ ": "weight lifting",
+  "🚴": "cycling",
+  "🤼": "wrestling"
+};
+
 export default function App() {
   const [input, setInput] = useState("");
 
   function activityHandler() {
-    var input = event.target.value;
-    setInput(input);
+    var name = event.target.value;
+    var act = activities[name];
+
+    setInput(act);
   }
 
   return (
     <div className="App">
       <h1>Activities</h1>
       <input onChange={activityHandler}></input>
-      <div>{input}</div>
+      <h2>{input}</h2>
     </div>
   );
 }
